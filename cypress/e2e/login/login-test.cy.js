60e2b7387b17a7bb9eefdb04365115d1bf6d2851
Rodrigo Alves Costa <rodrigo.costa@gmail.com>
2022-10-06 20:54:27 -0300

    testes adicionais de login

--- cypress/e2e/login/login-test.cy.js
+++ cypress/e2e/login/login-test.cy.js
@@ -2,7 +2,7 @@
 /// <reference types="cypress" />
 
 describe('testes de login', () => {
-    beforeEach(() => {
+    before(() => {
       cy.visit('http://webdriveruniversity.com/Login-Portal/index.html?')
     })
 
@@ -10,5 +10,23 @@ describe('testes de login', () => {
         cy.get('#text').should('be.visible');
         cy.get('#password').should('be.visible');
         cy.get('#login-button').should('not.be.disabled');
+    })
+
+    it('Deve aceitar tentativa de login valida', () => {
+        cy.get('#text').type('webdriver');
+        cy.get('#password').type('webdriver123')
+        cy.get('#login-button').click();
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('validation succeeded')
+          })
+    })
+
+    it('Nao deve aceitar tentativa de login invalida', () => {
+        cy.get('#text').type('enosdoido');
+        cy.get('#password').type('enosdoido123')
+        cy.get('#login-button').click();
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('validation failed')
+          })
     })
 })  
